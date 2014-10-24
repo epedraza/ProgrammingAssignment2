2b50d6e36d2dc5a1678e38b46ad0bbbe615b0576
Euler Pedraza <euler@cofradia.org>
2014-10-23 23:10:46 -0500

    Coursera R Programming: assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Use the rules of scope of R  and the operator <<-
+## to create an object (matrix like)
+## that can  store their inverse in cache
 
-## Write a short comment describing this function
+## Creates an object with associated functions to store and retrieve
+## the object itself and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(calculated) inverse <<- calculated
+    getinverse <- function() inverse
+    list(
+        set = set,
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse
+    )
 }
 
 
-## Write a short comment describing this function
+
+## Use the object defined before to find if the inverse has been calculated
+## returning this, if not then calculates the inverse, store in
+## their cache and return its value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(is.null(inverse)) {
+        message("calculating inverse")
+        inverse <- solve(x$get())
+        x$setinverse(inverse)
+    } else {
+        message("returning inverse from cache")
+    }
+    inverse
 }
